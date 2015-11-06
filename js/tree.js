2f3a20590f3f3dae2898af2480bfad03efd17e76
Erik816 <Erik.Christensen@gmail.com>
2015-11-06 11:12:29 -0800

    tree populating with JSON array

--- js/tree.js
+++ js/tree.js
@@ -4,15 +4,84 @@ $(document).ready(function () {
       "animation" : 0,
       "themes" : {
         "dots" : false,
-      }
+      },
+      "data" : [
+        {
+          "id" : "account",
+          "text" : "Erik.Christensen@gmail.com",
+          "state" : {
+            opened: true
+          },
+          children : [
+            {
+              "id" : "inbox",
+              "text" : "Inbox",
+              "state" : {
+                opened : true
+              },
+              children : [
+                {
+                  "id" : "drafts",
+                  "text" : "Drafts"
+                },
+                {
+                  "id" : "sent-mail",
+                  "text" : "Sent Mail"
+                },
+                {
+                  "id" : "all-mail",
+                  "text" : "All Mail"
+                },
+                {
+                  "id" : "spam",
+                  "text" : "Spam"
+                },
+                {
+                  "id" : "trash",
+                  "text" : "Trash"
+                },
+                {
+                  "id" : "important",
+                  "text" : "Important"
+                },
+                {
+                  "id" : "starred",
+                  "text" : "Starred"
+                }
+              ]
+            },
+            {
+              "id" : "deleted-items",
+              "text" : "Deleted Items"
+            },
+            {
+              "id" : "sent-items",
+              "text" : "Sent Items"
+            }
+          ]
+        },
+        {
+          "id" : "local-folders",
+          "text" : "Local Folders",
+          "state" : {
+            opened : true
+          },
+          children : [
+            {
+              "id" : "local-trash",
+              "text" : "Trash"
+            },
+            {
+              "id" : "local-outbox",
+              "text" : "Outbox"
+            }
+          ]
+        }
+      ]
     }
   });
   // Bind to events triggered on the tree
   $('#jstree').on("changed.jstree", function (e, data) {
     console.log(data.selected);
   });
-  // Interact with the tree
-  //$('inbox').on('click', function () {
-  //  $('#jstree').jstree(true).open_node('inbox');
-  //});
 });
