ee53fe2d8ff871f01791ef04caa913f3cfab1313
Erik816 <Erik.Christensen@gmail.com>
2015-11-06 16:13:09 -0800

    sample email data working in jsGrid

--- js/jqGrid.js
+++ js/jqGrid.js
@@ -1,12 +1,18 @@
+// jqGrid.js controls the initialization and settings of the main email pane. It uses the jqGrid plugin for jQuery, which can be found at http://www.trirand.com/blog/. It is currently hard-coded to accept a sample email JSON document, but can be configured to load from a server.
+
 $(document).ready(function () {
     $("#jqGrid").jqGrid({
         url: 'emailsample.json',
         datatype: "json",
           colModel: [
-            { label: 'Star', name: 'star', width: 10 },
-            { label: 'Subject', name: 'Subject', width: 75 },
-            { label: 'From', name: 'From', width: 90 },
-            { label: 'Date', name: 'Date', width: 100, sorttype: 'datetime', datefmt: 'm/d/Y' }
+            { label: '<span class="icon icon-flow-cascade"></span>', name: 'thread', width: 10, resizable: true},
+            { label: '<span class="icon icon-star"></span>', name: 'star', width: 10, resizable: true },
+            { label: '<span class="icon icon-attach"></span>', name: 'attachment', width: 10, resizable: true },
+            { label: 'Subject', name: 'Subject', width: 100, resizable: true },
+            { label: '<span class="icon icon-eye"></span>', name: 'Read', width: 10, resizable: true },
+            { label: 'From', name: 'From', width: 75, resizable: true },
+            { label: '<span class="icon icon-minus-circled"></span>', name: 'Spam', width: 10, resizable: true },
+            { label: 'Date', name: 'Date', width: 35, sorttype: 'datetime', datefmt: 'm/d/Y', resizable: true }
           ],
           viewrecords: true, // show the current page, data rang and total records on the toolbar
           autowidth: true,
@@ -33,7 +39,7 @@ $(document).ready(function () {
     resizeGrid('#jqGrid', '#email-pane');
   });
 
-  /**This function resizes the width of jQGrid, makes jqgrid responsive
+  /**This function resizes the width of jqGrid, makes jqGrid responsive
   * @param responsiveParentId Id of parent that is responsive, by default it will take body
   * @param idOfTable Id of the table that needs to be responsive
   *
